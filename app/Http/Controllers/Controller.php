a7c0842f4c0dec00373e4fe4348449c25ef5879b
ElenDS <dolena0308@gmail.com>
2023-03-23 23:30:27 +0200

    fixed controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,16 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Jobs\SendEmailJob;
-use App\Models\Ad;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Password;
 use Illuminate\View\View;
-use App\Mail\Mail as Test;
 
 class Controller extends BaseController
 {
@@ -21,9 +16,6 @@ class Controller extends BaseController
 
     public function show(): View
     {
-
-         SendEmailJob::dispatch('dolena0308@gmail.com', '1');
-        $ads = Ad::with(['user'])->get();
         return view('pages.registration');
     }
 }
